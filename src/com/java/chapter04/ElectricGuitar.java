4e9c26468557a3f899938b10f6b3dce7501d2893
John Doe <john@sample.com>
2016-12-25 01:28:18 +0800

    chapter04

---
+++ src/com/java/chapter04/ElectricGuitar.java
@@ -0,0 +1,31 @@
+package com.java.chapter04;
+
+/**
+ * Created by joanl on 12/23/2016.
+ */
+public class ElectricGuitar {
+    String bland;
+    int numOfPickups;
+    Boolean isRockStarUseIt;
+    String getBland(){
+        return bland;
+    }
+    void setBland(String abland){
+        bland=abland;
+    }
+
+int getNumOfPickups(){
+    return numOfPickups;
+}
+void setNumOfPickups(int anumOfPickups){
+    numOfPickups=anumOfPickups;
+}
+
+Boolean getIsRockStarUseIt(Boolean isRockStarUseIt){
+    return isRockStarUseIt;
+}
+
+void setIsRockStarUseIt(Boolean aisRockStarUseIt){
+    isRockStarUseIt=aisRockStarUseIt;
+}
+}
