6b71ea47b06fa1f979e26890c48d6ccafea1d1e0
lsq4590578 <lsq_4590578@126.com>
2017-01-03 17:45:26 +0800

    chapter05

---
+++ src/com/java/chapter05/TestSimpleDotCom1.java
@@ -0,0 +1,22 @@
+package com.java.chapter05;
+
+/**
+ * Created by joanl on 12/27/2016.
+ */
+public class TestSimpleDotCom1 {
+    public static void main(String[] agrs) {
+        SimpleDotCom2 simpledotcom = new SimpleDotCom2();
+        int[] locationcells = {2, 3, 4};
+        simpledotcom.setLocationcells(locationcells);
+                String guessnum = "3";
+        String result=simpledotcom.checkYouself(guessnum);
+        String testresult="fialed";
+        if(result.equalsIgnoreCase("hit")){
+            testresult="pass";
+        }
+        System.out.println(testresult);
+
+    }
+
+}
+
